4590100289a0e9bd7c41a2c9156f8e80a375beb8
mrcpp <gimrcpp@gmail.com>
2017-06-12 16:06:27 +0300

    changes

--- app/src/main/java/ru/dksta/prohibitingsigndetector/ActivitySettings.java
+++ app/src/main/java/ru/dksta/prohibitingsigndetector/ActivitySettings.java
@@ -2,12 +2,41 @@ package ru.dksta.prohibitingsigndetector;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
-public class ActivitySettings extends Activity {
+import ru.dksta.prohibitingsigndetector.utils.Prefs;
+
+public class ActivitySettings extends Activity implements View.OnClickListener,
+    CompoundButton.OnCheckedChangeListener {
+
+    private Prefs prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        prefs = new Prefs(getApplicationContext());
+        CheckBox rotation = (CheckBox) findViewById(R.id.rotation);
+        rotation.setChecked(prefs.getBoolean(Prefs.ROTATE_MAT));
+        rotation.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId()) {
+            case R.id.rotation:
+                prefs.putBoolean(Prefs.ROTATE_MAT, isChecked);
+                break;
+        }
     }
 }
